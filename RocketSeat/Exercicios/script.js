45383797cb7d0058b85fd9c60e9f4a38b42574ea
Patrick Borges Martins <patrickmartins89@hotmail.com>
2021-12-05 10:11:54 -0300

    Update script.js

--- RocketSeat/Exercicios/script.js
+++ RocketSeat/Exercicios/script.js
@@ -90,3 +90,46 @@ try {
 }
 
 //Buscando e contando dados em array
+
+const booksByCategory = [
+    {
+        category: "Riqueza",
+        books: [
+            {
+                title: "Os segredos da mente milionária",
+                author: "T. Harv Eker",
+            },
+            {
+                title: "O homem mais rico da Babilônia",
+                author: "George S. clason",
+            },
+            {
+                title: "Pai rico, pai pobre",
+                author: "Robert T. Kiyosaki e Sharon L. Lechter",
+            },
+        ],
+    },
+    {
+        category: "Inteligência Emocional",
+        books: [
+            {
+                title: "Você é insubstituível",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Ansiedade - Como enfrentar o mal do século",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Os 7 hábitos das pessoas altamente eficazes",
+                author: "Steven R. Covey",
+            },
+        ],
+    },
+];
+
+const totalCategories = booksByCategory.length
+
+for(let category of booksByCategory) {
+    console.log(category)
+}

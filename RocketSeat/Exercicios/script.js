344f4bf2a2862374cfa7a039349eb4e99953d227
Patrick Borges Martins <patrickmartins89@hotmail.com>
2021-12-05 09:47:06 -0300

    Update script.js

--- RocketSeat/Exercicios/script.js
+++ RocketSeat/Exercicios/script.js
@@ -23,3 +23,70 @@ for (let property in receitas && despesas) {
     }
 }
 
+// Correção
+let family = {
+    incomes: [2500, 3200, 210, 500.50],
+    expenses: [2700, 500, 300]
+}
+
+function sum(array) {
+    let total = 0;
+
+    for (let value of array) {
+        total += value
+    }
+
+    return total
+
+}
+
+function calculateBalance() {
+    const calculateIncomes = sum(family.incomes)
+    const calculateExpenses = sum(family.expenses)
+
+    const total = calculateIncomes - calculateExpenses
+
+    const itsOk = total >= 0
+
+    let balanceText = 'negativo'
+
+    if (itsOk) {
+        balanceText = 'positivo'
+    }
+
+    console.log(`Seu saldo é ${balanceText}: ${total.toFixed(2)}R$`)
+}
+
+calculateBalance()
+
+//Celcius em Fahrenheit
+
+function transformDegree(degree) {
+    const celciusExists = degree.toUpperCase().includes('C')
+    const fahrenheitExists = degree.toUpperCase().includes('F')
+
+    //fluxo de erro
+    if (!celciusExists && !fahrenheitExists) {
+        throw new Error('Grau não identificado')
+    }
+    // fluxo ideal, F -> C
+    let updateDegree = Number(degree.toUpperCase().replace("F", ""));
+    let formula = fahrenheit => (fahrenheit - 32) * 5 / 9
+    let degreeSign = 'C'
+
+    //fluxo alternativo C -> F
+    if (celciusExists) {
+        updateDegree = Number(degree.toUpperCase().replace("C", ""));
+        formula = celcius => celcius * 9 / 5 + 32
+        degreeSign = 'F'
+    }
+    return formula(updateDegree) + degreeSign
+}
+try {
+    console.log(transformDegree('50F'))
+    //transformDegree('50Z')
+} catch (error) {
+    console.log(error.message)
+}
+
+//Buscando e contando dados em array

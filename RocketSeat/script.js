7d1c9a3a636ab2af4f386c5c55055dcd92a93662
Patrick Borges Martins <patrickmartins89@hotmail.com>
2021-12-14 16:34:05 -0300

    estudo

--- RocketSeat/script.js
+++ RocketSeat/script.js
@@ -228,6 +228,21 @@ for(let property in person) {
     console.log(person[property])
 }
 
+//Dispara um evento
+const { inherits } = require('util')
+const { EventEmitter } = require('events')
+
+function Character(name) {
+    this.name = name
+}
+
+inherits(Character, EventEmitter)
+
+const chapolin = new Character('Chapolin');
+chapolin.on('help', () => console.log(`Eu! O ${chapolin.name} colorado!`))
+
+console.log('Oh! E agora, quem poderá me defender?')
+chapolin.emit('help')
 
 
 
